c4f3c2eff1dc8242e99c461ddf7e73b891992207
Tomasz Omeljaniuk <tomekomel@gmail.com>
2024-09-12 17:15:42 +0200

    feat: Add jwt-auth guard

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,13 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.connectMicroservice<MicroserviceOptions>({ transport: Transport.TCP });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
