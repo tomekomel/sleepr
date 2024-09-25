f65b38827a43a6fa10d4e722c7f769862e30126e
Tomasz Omeljaniuk <tomekomel@gmail.com>
2024-09-25 15:22:35 +0200

    feat: Simplify guard

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -4,7 +4,7 @@ import {
   Inject,
   Injectable,
 } from '@nestjs/common';
-import { map, Observable, tap } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 import { AUTH_SERVICE } from '@app/common/constants/services';
 import { ClientProxy } from '@nestjs/microservices';
 import { UserDto } from '@app/common/dto';
@@ -12,21 +12,20 @@ import { UserDto } from '@app/common/dto';
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
   constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const jwt = context.switchToHttp().getRequest().cookies?.Authentication;
     if (!jwt) {
       return false;
     }
 
-    return this.authClient
-      .send<UserDto>('authenticate', { Authentication: jwt })
-      .pipe(
-        tap((res) => {
-          context.switchToHttp().getRequest().user = res;
-        }),
-        map(() => true),
+    try {
+      context.switchToHttp().getRequest().user = await lastValueFrom(
+        this.authClient.send<UserDto>('authenticate', { Authentication: jwt }),
       );
+      return true;
+    } catch (error) {
+      console.error('Authentication error:', error);
+      return false;
+    }
   }
 }
