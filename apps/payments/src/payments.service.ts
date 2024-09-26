dbab3edacb3f00949247539acf3730bf4e862c9a
Tomasz Omeljaniuk <tomekomel@gmail.com>
2024-09-26 10:49:33 +0200

    feat: Install stripe package

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PaymentsService {
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+    { apiVersion: '2024-06-20' },
+  );
+
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
