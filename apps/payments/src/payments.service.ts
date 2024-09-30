2209cee256b43de0f577dc0f079bf0c35f330fe0
Tomasz Omeljaniuk <tomekomel@gmail.com>
2024-09-30 13:00:41 +0200

    feat: Automatic test visa payments

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -12,18 +12,13 @@ export class PaymentsService {
 
   constructor(private readonly configService: ConfigService) {}
 
-  async createCharge({ card, amount }: CreateChargeDto) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
-    });
-
+  async createCharge({ amount }: CreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
-      payment_method: paymentMethod.id,
+      payment_method: 'pm_card_visa',
       amount: amount * 100,
       confirm: true,
-      payment_method_types: ['card'],
       currency: 'usd',
+      automatic_payment_methods: { enabled: true, allow_redirects: 'never' },
     });
 
     return paymentIntent;
