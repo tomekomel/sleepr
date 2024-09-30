63c2fc0c1da4c692e8e8e6030ac694995d9d856f
Tomasz Omeljaniuk <tomekomel@gmail.com>
2024-09-30 13:00:57 +0200

    Revert "feat: Automatic test visa payments"
    This reverts commit 2209cee256b43de0f577dc0f079bf0c35f330fe0.

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -12,13 +12,18 @@ export class PaymentsService {
 
   constructor(private readonly configService: ConfigService) {}
 
-  async createCharge({ amount }: CreateChargeDto) {
+  async createCharge({ card, amount }: CreateChargeDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
     const paymentIntent = await this.stripe.paymentIntents.create({
-      payment_method: 'pm_card_visa',
+      payment_method: paymentMethod.id,
       amount: amount * 100,
       confirm: true,
+      payment_method_types: ['card'],
       currency: 'usd',
-      automatic_payment_methods: { enabled: true, allow_redirects: 'never' },
     });
 
     return paymentIntent;
